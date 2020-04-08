a61b8576aeb239bf135628a54cdd51b353fe11f8
Claude Gauthier <claude_r_gauthier@hotmail.com>
2020-04-08 15:05:22 -0400

    added more to function_arguments

--- basic_tutorial/function_arguments.py
+++ basic_tutorial/function_arguments.py
@@ -25,9 +25,19 @@ def kwd_only_arg(*, arg):
 def combined_example(pos_only, /, standard, *, kwd_only):
     print(pos_only, standard, kwd_only)
 
-
+# demonstrating how to use multiple keyword only in a way which shows that position isn't important
 def multiple_kwd_only_arg(*, arg, arg2, arg3):
     print(arg, "\n", arg2, "\n", arg3)
+
+# technically, we can have a named argument that can also be positional and yet have it passed as part of kwds.
+# using ** to avoid collusion for keyword only arguments.
+def avoid_combined_keyword_name_collusion(name, /, **kwds):
+    # this gets name as part of the function definition
+    print("name: ", name);
+    for kwd in kwds:
+        print(kwd, ":", kwds[kwd])
+
+
 
 
 print("\nUsing a standard function argument\n")
@@ -66,3 +76,36 @@ print("\nUsing keyword only function with multiple arguments\n")
 
 multiple_kwd_only_arg(arg3="args do not need to be in order", arg2="world", arg="hello")
     
+
+print("\nUsing ** to avoid name collusion in keyword name arguments\n")
+
+avoid_combined_keyword_name_collusion("claude", **{'name': "foo"}, other = "bar")
+
+
+print("\nUsing Join\n")
+
+def concat(*args, sep="/"):
+    return sep.join(args)
+
+concat("earth", "wind", "fire")    
+
+
+print("\nUsing Range\n")
+
+res = list(range(3, 6))
+print(res);
+
+args = [3, 6]
+res = list(range(*args))
+print(res)
+
+
+print("\nUsing Dictonary\n")
+
+def parrot(voltage, state="a stiff", action="voom"):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.", end= ' ')
+    print("E's", state, "!")
+
+d = {"voltage": "four million", "state": "bleedin' demised", "action": "VOOM"}
+parrot(**d)
